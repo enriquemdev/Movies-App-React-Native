f388db012e9eef8dea3a983199758d380adad73b
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-05-11 12:39:23 -0600

    configured tailwind custom colors

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,11 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      // style={{
-      //   flex: 1,
-      //   justifyContent: "center",
-      //   alignItems: "center",
-      // }}
         className="flex-1 justify-center items-center"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+          className="text-3xl text-dark-200 font-bold text-center"
+      >Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
